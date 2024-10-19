4c03484aff6ac0ede0fefed529dc6a29c1bc8dec
okumu87 <okumu793@gmail.com>
2024-10-19 21:01:06 +0200

    code reviewed

--- script.js
+++ script.js
@@ -14,13 +14,18 @@ function imageMode(color) {
   image3.scr = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-function toggleDarkLightMode(isDark){
-  nav.style.backgroundColor = isDark ? "rgb(0 0 0 / 50%)": rgb(255 255 255 / 50%);
-  textBox.style.backgroundColor = isDark ? "rgb(255 255 255 / 50%)" : rgb(0 0 0 / 50%);
-  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : 'Light Mode';
-  isDark ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon") : 
-  toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  isDark ? imageMode('dark') : imageMode('light');
+function toggleDarkLightMode(isDark) {
+  nav.style.backgroundColor = isDark
+    ? "rgb(0 0 0 / 50%)"
+    : "rgb(255 255 255 / 50%)";
+  textBox.style.backgroundColor = isDark
+    ? "rgb(255 255 255 / 50%)"
+    : " rgb(0 0 0 /50%)";
+  toggleIcon.children[0].textContent = isDark ? "Dark Mode" : "Light Mode";
+  isDark
+    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
+    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
+  isDark ? imageMode("dark") : imageMode("light");
 }
 
 // //Dark Mode Styles
@@ -47,11 +52,11 @@ function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
-    toggleDarkLightMode(true)
+    toggleDarkLightMode(true);
   } else {
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
-    toggleDarkLightMode(false)
+    toggleDarkLightMode(false);
   }
 }
 // Event Listener
